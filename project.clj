7bc9d7e80d4dcffe7ae7aa10253f3f462071f2b4
Robert Boyd <robert.boyd@me.com>
2013-01-30 21:01:35 -0600

    [Finishes #1] Identify the best n card hand from a set of p cards

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :author "James MacAulay"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/math.combinatorics "0.0.3"]])
